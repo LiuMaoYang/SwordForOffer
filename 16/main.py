06ee2a5f0c4b023860aff3718d560214f10d978e
LiuMaoYang <maoliu_yang@hnu.edu.cn>
2017-06-29 20:43:01 +0800

    合并链表

--- 16/main.py
+++ 16/main.py
@@ -1,75 +1,44 @@
-#encoding:utf-8
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+# -*- coding:utf-8 -*-
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        
+    def CreateList(self,list,data):
+        temp1=list
+        temp1.val=data[0]
+        for i in data[1:]:
+            temp2=ListNode(i)
+            temp1.next=temp2
+            temp1=temp1.next
 
-path='F:/Desktop/1.JPG'
-img=cv2.imread(path)
-imgGray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    def Merge(self, pHead1, pHead2):
+        if pHead1==None and pHead2==None:
+            return None
+        elif pHead1==None:
+            return pHead2
+        elif pHead2==None:
+            return pHead1
+        
+        if pHead1.val>pHead2.val:
+            pHead2.next=self.Merge(pHead1,pHead2.next)
+            return pHead2
+        else:
+            pHead1.next=self.Merge(pHead1.next,pHead2)
+            return pHead1
 
-# Splitting and Merging Image Channels
-#Note: cv: RGB  plt: BGR
-r,g,b=cv2.split(img)
-imgPlt=cv2.merge([b,g,r])
-#the operation same as
-imgPlt=cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+data1=raw_input().split(' ')   
+l1=ListNode(None)
+l1.CreateList(l1, data1)
 
-plt.subplot(1,2,1),plt.imshow(imgPlt),plt.title('RGB')
-plt.subplot(1,2,2),plt.imshow(imgGray,cmap='gray'),plt.title('Gray')
-plt.show()
-  
-cv2.namedWindow('RGB'),cv2.imshow('RGB',img)
-cv2.namedWindow('Gray'),cv2.imshow('Gray',imgGray)
-cv2.waitKey(),cv2.destroyAllWindows()
+data2=raw_input().split(' ')   
+l2=ListNode(None)
+l2.CreateList(l2, data2)
 
-# Accessing and Modifying pixel values
-print img.item(1,1,1)#157
-img.itemset((1,1,1),100)
-print img.item(1,1,1)#100
+newL=ListNode(None)
+newL=newL.Merge(l1, l2)
+while newL:
+    print newL.val,
+    newL=newL.next
 
-# Accessing Image Properties
-print img.shape #(396L, 687L, 3L)
-print img.size #816156 (total number of pixels)
-print img.dtype #uint8
-
-#ROI
-ROI=img[130:200,80:190]
-imgAdd=img
-imgAdd[290:290+ROI.shape[0],190:190+ROI.shape[1]]=ROI
-cv2.namedWindow('AddROI'),cv2.imshow('AddROI',imgAdd)
-cv2.waitKey(),cv2.destroyAllWindows()
-
-#Padding
-img1=imgPlt
-r,c=img1.shape[0],img1.shape[1]
-step=int(raw_input())
-top=int(r/step)
-bottom=int(r/step)
-left=int(c/step)
-right=int(c/step)
-
-# cv2.BORDER_REPLICATE - Last element is replicated throughout:
-# aaaaaa|abcdefgh|hhhhhhh
-replicate = cv2.copyMakeBorder(img1,top,bottom,left,right,cv2.BORDER_REPLICATE)
-# cv2.BORDER_REFLECT - Border will be mirror reflection of the border elements: 
-# fedcba|abcdefgh|hgfedcb
-reflect = cv2.copyMakeBorder(img1,top,bottom,left,right,cv2.BORDER_REFLECT)
-# cv2.BORDER_REFLECT_101 or cv2.BORDER_DEFAULT - Same as above, but with a slight change: 
-# gfedcb|abcdefgh|gfedcba
-reflect101 = cv2.copyMakeBorder(img1,top,bottom,left,right,cv2.BORDER_REFLECT_101)
-# cv2.BORDER_WRAP - Can’t explain, it will look like this : 
-# cdefgh|abcdefgh|abcdefg
-wrap = cv2.copyMakeBorder(img1,top,bottom,left,right,cv2.BORDER_WRAP)
-# cv2.BORDER_CONSTANT - Adds a constant colored border. The value should be given as next
-# argument.
-constant= cv2.copyMakeBorder(img1,top,bottom,left,right,cv2.BORDER_CONSTANT,value=[255,0,0])
-
-plt.subplot(231),plt.imshow(img1,cmap='gray'),plt.title('ORIGINAL')
-plt.subplot(232),plt.imshow(replicate,'gray'),plt.title('REPLICATE')
-plt.subplot(233),plt.imshow(reflect,'gray'),plt.title('REFLECT')
-plt.subplot(234),plt.imshow(reflect101,'gray'),plt.title('REFLECT_101')
-plt.subplot(235),plt.imshow(wrap,'gray'),plt.title('WRAP')
-plt.subplot(236),plt.imshow(constant,'gray'),plt.title('CONSTANT')
-plt.show()
-
-
+            
